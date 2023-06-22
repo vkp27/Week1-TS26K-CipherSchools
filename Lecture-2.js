4968c6f43d89341c02f20320399d670eb0d6ca25
vaibhav <vaibhavkrpandey111@gmail.com>
2023-06-23 00:25:47 +0530

    Updated Lec2 Code

---
+++ Lecture-2.js
@@ -0,0 +1,44 @@
+// #Strings (String and string)
+
+    let number = "raining"; // String.prototype 
+    let string = "String as a name"
+
+    let String= 20083838;
+
+    console.log(word.toUpperCase());
+    console.log(String);
+
+    console.log('hello and good evening'.length)
+    console.log("hello".charAt(4))
+
+// #Boolean (true, 1, false, 0)
+
+    console.log(typeof true);
+    console.log(typeof false);
+    console.log(isNaN('five'));
+
+    // Truthy Values: any value 
+    // Falsy Values: 0 false '' NaN undefined null
+
+    let condition = 10/'hello'; // NaN
+    if(condition){
+        console.log("Very true!!")
+    }else{
+        console.log("very wrong!!")
+    }
+
+    console.log('hello'.includes('e'));
+
+
+// # Null Undefined 
+    let score;
+    let result = null;
+
+        if(result){
+            console.log("Very true!!")
+        }else{
+            console.log("very wrong!!")
+        }
+
+    console.log(typeof score);
+    console.log(typeof result);
